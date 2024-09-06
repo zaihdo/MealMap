0583c3ad9d98c7b8a5dba7a8292f78ba73932271
Zaidh Imran <zaidhimran2000@gmail.com>
2024-09-06 12:06:35 -0230

    explore screen bg color change

--- MealMap/app/(tabs)/explore.tsx
+++ MealMap/app/(tabs)/explore.tsx
@@ -11,7 +11,7 @@ import styles from '@/components/styles';
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={{ light: '#E4CDB1', dark: '#E4CDB1' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Explore</ThemedText>
